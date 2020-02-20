44311141d17bc1c0d3d40d79fe207c895d602205
William Locatelli <wheretheresawilll@gmail.com>
2020-02-20 15:43:37 -0800

    cleaned up and generalized code, centered graph

--- MarvelTracker.py
+++ MarvelTracker.py
@@ -3,8 +3,10 @@ import csv
 
 root = []
 
+SQUARE_SIZE = 5
 
-def import_movies(file="AdjacencyList.csv"):
+
+def import_movies_from_csv(file="AdjacencyList.csv"):
     # put all the rows of the csv file into a list
     movies_list = []
     with open(file, newline='', encoding='utf-8-sig') as csvfile:
@@ -12,13 +14,13 @@ def import_movies(file="AdjacencyList.csv"):
         for row in reader:
             movies_list.append(row)
     result = {}
-    x = 10
-    y = 150
+    # initial position
     layer = 0
+    # iterate through the list of movies, creating an object for each
     for movie in movies_list:
-        this_movie = Movie(Point(x, y), movie[0])
-        result[movie[0]] = this_movie
-        # add prevs
+        this_movie = Movie(movie[0])
+        result[movie[0]] = this_movie  # add the movie to a dictionary with the name as the key
+        # generate adjacency list of previous movies
         prevs = []
         # keep track of layers
         previous_layer = []
@@ -30,37 +32,42 @@ def import_movies(file="AdjacencyList.csv"):
         # increment the layers if necessary
         if len(previous_layer) > 0 and layer <= max(previous_layer):
             layer = layer + 1
-            x = 10
-            y = y - 10
-            this_movie.set_point(Point(x, y))
         this_movie.layer = layer
-        # update coordinates for next movie
-        x = x + 20
-        '''if x > 80:
-            x = 10
-            y = y - 20'''
-    # add nexts:
+    # add nexts
     for movie in result.values():
         if len(movie.prevs) > 0:
             for prev in movie.prevs:
                 prev.seqs.append(movie)
         else:
             movie.prevs_selected = True
-
+    # give each movie a coordinate
+    handle_positions(result)
     return result
+
+
+def handle_positions(movies):
+    layers = {}
+    for movie in movies.values():
+        if layers.__contains__(movie.layer):
+            layers[movie.layer].append(movie)
+        else:
+            layers[movie.layer] = [movie]
+    y = 150
+    for layer in layers.values():
+        size = len(layer)
+        pos = 100 / (size + 1)
+        x = pos
+        for movie in layer:
+            movie.set_point(Point(x - SQUARE_SIZE/2, y))
+            x = x + pos
+        y = y - 10
 
 
 # test
 def make_window(movies):
-    '''avengers = Movie(Point(10, 20), "The Avengers")
-    avengers.draw()
-    captainamerica = Movie(Point(25, 50), "Captain America")
-    captainamerica.draw()
-    ironman = Movie(Point(80, 20), "Iron Man")
-    ironman.draw()
-    movies = [avengers, captainamerica, ironman]'''
     for movie in movies.values():
         movie.draw()
+        # draw lines connecting movies
         '''if len(movie.prevs) > 0:
             for prev in movie.prevs:
                 line = Line(movie.p2, prev.p1)
@@ -70,58 +77,67 @@ def make_window(movies):
         p = Movie.win.getMouse()
         for movie in movies.values():
             if movie.p1.getX() < p.getX() < movie.p2.getX() and movie.p1.getY() < p.getY() < movie.p2.getY():
-                if movie.prevs_selected is True:
+                if movie.prevs_selected is True:  # proceed only if the prevs of this movie have been selected
                     if movie.selected is False:
-                        movie.selected = True
-                        movie.mySquare.setFill(color_rgb(125, 25, 32))
-                        for seq in movie.seqs:
-                            prevs_selected = True
-                            for prev in seq.prevs:
-                                if not prev.selected is True:
-                                    prevs_selected = False
-                            if prevs_selected is True:
-                                seq.prevs_selected = True
-                                seq.mySquare.setFill("white")
+                        select_movie(movie)
                     else:
-                        movie.mySquare.setFill("white")
+                        movie.my_square.setFill("white")
                         movie.selected = False
                         clear_nexts(movie)
 
+
+# recursively marks all of the passed movie's children as unwatchable
 def clear_nexts(movie):
     for seq in movie.seqs:
         seq.selected = False
         seq.prevs_selected = False
-        seq.mySquare.setFill("gray")
+        seq.my_square.setFill("gray")
         clear_nexts(seq)
+
+
+# marks a movie as watched and updates its children as watchable, if permissible
+def select_movie(movie):
+    movie.selected = True
+    movie.my_square.setFill(color_rgb(125, 25, 32))
+    for seq in movie.seqs:
+        prevs_selected = True
+        for prev in seq.prevs:
+            if not prev.selected:
+                prevs_selected = False
+        if prevs_selected:
+            seq.prevs_selected = True
+            seq.my_square.setFill("white")
+
+
 # create a class for movies. each movie will be at a certain point and have a name. clicking on the movie will make it
 # do stuff.
 class Movie:
     win = GraphWin(width=500, height=800, title="MCU Tracker", autoflush=False)
     win.setCoords(0, 0, 100, 160)
 
-    def __init__(self, point, name):
-        self.selected = False
-        self.prevs = []
-        self.seqs = []
-        self.p1 = Point(point.getX(), point.getY())
-        self.p2 = Point(point.getX() + 5, point.getY() + 5)
-        self.mySquare = Rectangle(self.p1, self.p2)
-        self.name = name
-        self.prevs_selected = False
+    def __init__(self, name):
+        self.selected = False  # whether or not this movie has been marked as watched
+        self.prevs = []  # the parents of this movie
+        self.seqs = []  # the children of this movie
+        self.p1 = None
+        self.p2 = None
+        self.my_square = None
+        self.name = name  # the name of the movie
+        self.prevs_selected = False  # whether or not all of this movies parents have been marked as watched
 
     def draw(self):
-        self.mySquare.draw(self.win) # draw it to the window
-        if len(self.prevs) > 0:
-            self.mySquare.setFill("gray")
-        title = Text(self.mySquare.getCenter(), self.name)
+        self.my_square.draw(self.win)  # draw it to the window
+        if not self.prevs_selected:
+            self.my_square.setFill("gray")
+        title = Text(self.my_square.getCenter(), self.name)
         title.draw(self.win)
 
     def set_point(self, point):
         self.p1 = Point(point.getX(), point.getY())
-        self.p2 = Point(point.getX() + 5, point.getY() + 5)
-        self.mySquare = Rectangle(self.p1, self.p2)
+        self.p2 = Point(point.getX() + SQUARE_SIZE, point.getY() + SQUARE_SIZE)
+        self.my_square = Rectangle(self.p1, self.p2)
 
 
 if __name__ == '__main__':
-    movies = import_movies()
-    make_window(movies)
+    all_movies = import_movies_from_csv()
+    make_window(all_movies)
