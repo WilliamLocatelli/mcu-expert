47a0c22070e848ca7be7b4a19b66f14d64e43f64
William Locatelli <wheretheresawilll@gmail.com>
2020-02-20 22:31:52 -0800

    added rank swapping functionality; improved appearance of GUI

--- MarvelTracker.py
+++ MarvelTracker.py
@@ -4,6 +4,8 @@ import csv
 root = []
 
 SQUARE_SIZE = 5
+RECT_HEIGHT = 7
+RECT_WIDTH = 20
 BIG_X = 0
 BIG_Y = 0
 
@@ -13,7 +15,7 @@ def motion(event):
     global BIG_X, BIG_Y
     BIG_X, BIG_Y = event.x, event.y
     for movie in Movie.open:
-        if movie.p1.getX() < BIG_X/5 < movie.p2.getX() and movie.p1.getY() < 120 - (BIG_Y/5) < movie.p2.getY():
+        if movie.p1.getX() < BIG_X/5 < movie.p2.getX() and movie.p1.getY() < 140 - (BIG_Y/5) < movie.p2.getY():
             movie.my_square.setFill("blue")
         elif movie is Movie.first:
             movie.my_square.setFill("green")
@@ -34,14 +36,14 @@ def import_movies_from_csv(file="AdjacencyList.csv"):
     layer = 0
     # iterate through the list of movies, creating an object for each
     for movie in movies_list:
-        this_movie = Movie(movie[1], int(movie[0]))
-        result[movie[1]] = this_movie  # add the movie to a dictionary with the name as the key
+        this_movie = Movie(movie[3], int(movie[0]), int(movie[1]), int(movie[2]))
+        result[movie[3]] = this_movie  # add the movie to a dictionary with the name as the key
         # generate adjacency list of previous movies
         prevs = []
         # keep track of layers
         previous_layer = []
         for prev in movie:
-            if not prev == movie[0] and not prev == movie[1] and len(prev) > 1:
+            if not prev == movie[0] and not prev == movie[1] and not prev == movie[2] and not prev == movie[3] and len(prev) > 1:
                 prevs.append(result.get(prev))
                 previous_layer.append(result.get(prev).layer)
         this_movie.prevs = prevs
@@ -69,15 +71,15 @@ def handle_positions(movies):
             layers[movie.layer].append(movie)
         else:
             layers[movie.layer] = [movie]
-    y = 110
+    y = 130
     for layer in layers.values():
         size = len(layer)
         pos = 100 / (size + 1)
         x = pos
         for movie in layer:
-            movie.set_point(Point(x - SQUARE_SIZE/2, y))
+            movie.set_point(Point(x - RECT_WIDTH/2, y))
             x = x + pos
-        y = y - 1.5*SQUARE_SIZE
+        y = y - 1.5*RECT_HEIGHT
 
 
 # test
@@ -113,7 +115,11 @@ def color_best_movie(movies):
     max_movie = None
     for open_movie in Movie.open:
         open_movie.my_square.setFill("white")
-        r = open_movie.rank
+        if not movies.get("The Avengers").selected:
+            r = open_movie.rank1
+        elif not movies.get("Avengers: Age of Ultron").selected:
+            r = open_movie.rank2
+        else: r = open_movie.rank3
         if r <= max_rank:
             max_rank = r
             max_movie = open_movie
@@ -136,7 +142,7 @@ def clear_nexts(movie):
 def select_movie(movie):
     Movie.open.remove(movie)
     movie.selected = True
-    movie.my_square.setFill(color_rgb(125, 25, 32))
+    movie.my_square.setFill(color_rgb(160, 17, 27))
     for seq in movie.seqs:
         prevs_selected = True
         for prev in seq.prevs:
@@ -153,10 +159,10 @@ def select_movie(movie):
 class Movie:
     first = None
     open = []
-    win = GraphWin(width=500, height=600, title="MCU Tracker", autoflush=False)
-    win.setCoords(0, 0, 100, 120)
+    win = GraphWin(width=500, height=700, title="MCU Tracker", autoflush=False)
+    win.setCoords(0, 0, 100, 140)
 
-    def __init__(self, name, rank=0):
+    def __init__(self, name, rank1=0, rank2=0, rank3=0):
         self.selected = False  # whether or not this movie has been marked as watched
         self.prevs = []  # the parents of this movie
         self.seqs = []  # the children of this movie
@@ -165,7 +171,9 @@ class Movie:
         self.my_square = None
         self.name = name  # the name of the movie
         self.prevs_selected = False  # whether or not all of this movies parents have been marked as watched
-        self.rank = rank
+        self.rank1 = rank1
+        self.rank2 = rank2
+        self.rank3 = rank3
 
     def draw(self):
         self.my_square.draw(self.win)  # draw it to the window
@@ -173,12 +181,20 @@ class Movie:
             self.my_square.setFill("gray")
         else:
             Movie.open.append(self)
+        # work back from character 16 to find the last space before character 16
+        if len(self.name) > 16:
+            n = 16
+            k = self.name[n]
+            while not k == " ":
+                n -= 1
+                k = self.name[n]
+            self.name = self.name[:n] + "\n" + self.name[n:]
         title = Text(self.my_square.getCenter(), self.name)
         title.draw(self.win)
 
     def set_point(self, point):
         self.p1 = Point(point.getX(), point.getY())
-        self.p2 = Point(point.getX() + SQUARE_SIZE, point.getY() + SQUARE_SIZE)
+        self.p2 = Point(point.getX() + RECT_WIDTH, point.getY() + RECT_HEIGHT)
         self.my_square = Rectangle(self.p1, self.p2)
 
 
