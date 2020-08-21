19fd8243f735c422cea11dc3ccb9c09066cf0d3d
williamlocatelli <locatw@uw.edu>
2020-08-21 15:08:48 -0700

    error handling

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/*const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});*/
 const path = require('path')
 const express = require('express');
 const {spawn} = require('child_process');
@@ -14,8 +21,12 @@ app.use(multer().none());
 
 app.get('/main/', async function(req, res) {
     try {
+        //const client = await pool.connect();
+        //const titles = await client.query("SELECT title, series, release_date FROM films ORDER BY series");
+        //client.release();
         let db = await getDBConnection();
         let titles = await db.all("SELECT Title, Series, ReleaseDate FROM Films ORDER BY Series");
+        await db.close();
         let result = {"All Series": []};
         let series = 0;
         for (let i = 0; i < titles.length; i++) {
@@ -37,22 +48,25 @@ app.get('/main/', async function(req, res) {
 });
 
 app.post('/results/', async function (req, res) {
-    const python = spawn('python', [path.join(__dirname, 'LaunchScript.py'), req.body.options]);
-    let dataToSend = "no data";
-    python.stdout.on('data', (data) => {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-
-    python.stderr.on('data', (data) => {
-        console.log(`error:${data}`);
-    });
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to frontend
-        res.text;
-        res.send(dataToSend.substring(0, dataToSend.length - 1));
-    });
+    try {
+        const python = spawn('python', [path.join(__dirname, 'LaunchScript.py'), req.body.options]);
+        let dataToSend = "[no data]";
+        python.stdout.on('data', (data) => {
+            console.log('Pipe data from python script ...');
+            dataToSend = data.toString();
+        });
+        python.stderr.on('data', (data) => {
+            console.log(`error:${data}`);
+        });
+        python.on('close', (code) => {
+            console.log(`child process close all stdio with code ${code}`);
+            // send data to frontend
+            res.text;
+            res.send(dataToSend.substring(0, dataToSend.length - 1));
+        });
+    } catch(err) {
+        res.status(500).json({'error': 'Internal server error'});
+    }
 });
 
 async function getDBConnection() {
