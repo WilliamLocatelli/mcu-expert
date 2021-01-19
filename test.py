2727e8ba944cf753e33e35ac5470bfe1d5414a56
williamlocatelli <locatw@uw.edu>
2021-01-18 20:08:01 -0800

    +test functions

--- test.py
+++ test.py
@@ -10,81 +10,75 @@ def get_names(films):
         film_list.append(film.name)
     return str(film_list)
 
-Rec.import_weighted_from_csv()
-Rec.import_data_from_csv()
-Rec2.import_weighted_from_csv()
-Rec2.import_data_from_csv()
+def test_all_films():
+    all_films = []
+    for movie in Rec.MOVIES:
+        all_films.append(movie)
+    rules = ["Relevant", "Interconnected", "Recent"]
 
-all_films = []
-for movie in Rec.MOVIES:
-    all_films.append(movie)
+    # generate random movie lists
+    are_same = True
+    for i in range(10000):
+        rule = rules[math.floor(random() * 3)]
+        #rule = "Relevant"
+        #if i % 2 == 0:
+        #    rule = "Recent"
+        Rec.set_rule(rule)
+        Rec2.set_rule(rule)
+        films = all_films.copy()
+        watched1 = []
+        children1 = []
+        watched2 = []
+        children2 = []
+        num_watched = math.floor(random() * len(all_films) / 3)
+        num_children = 1 + math.floor(random() * len(all_films) / 3)
+        num_extras = math.floor(random() * len(all_films) / 3)
+        # num_watched = 2
+        # num_children = 2
+        # num_extras = 2
+        for num in range(num_watched):
+            n = math.floor(random() * len(films))
+            watched1.append(Rec.MOVIES[films[n]])
+            watched2.append(Rec2.MOVIES[films[n]])
+            films.remove(films[n])
 
-rules = ["Relevant", "Interconnected", "Recent"]
+        for num in range(num_children):
+            n = math.floor(random() * len(films))
+            children1.append(Rec.MOVIES[films[n]])
+            children2.append(Rec2.MOVIES[films[n]])
+            films.remove(films[n])
 
-# generate random movie lists
-are_same = True
-for i in range(10000):
-    #rule = rules[math.floor(random() * 3)]
-    rule = "Relevant"
-    if i % 2 == 0:
-        rule = "Recent"
-    Rec.set_rule(rule)
-    Rec2.set_rule(rule)
-    films = all_films.copy()
-    watched = []
-    children = []
-    #num_watched = math.floor(random() * len(all_films) / 3)
-    #num_children = 1 + math.floor(random() * len(all_films) / 3)
-    #num_extras = math.floor(random() * len(all_films) / 3)
-    num_watched = 2
-    num_children = 2
-    num_extras = 2
-    for num in range(num_watched):
-        n = math.floor(random() * len(films))
-        watched.append(Rec.MOVIES[films[n]])
-        films.remove(films[n])
+        graph1 = Rec.find_best_subgraph(watched1, children1, num_extras)
+        graph2 = Rec2.find_best_subgraph(watched2, children2, num_extras)
 
-    for num in range(num_children):
-        n = math.floor(random() * len(films))
-        children.append(Rec.MOVIES[films[n]])
-        films.remove(films[n])
-
-    graph2 = Rec2.find_best_subgraph(watched, children, num_extras)
-    graph1 = Rec.find_best_subgraph(watched, children, num_extras)
-
-    # compare the graphs
-    if len(graph1) != len(graph2):
-        are_same = False
-        print("Failed after " + str(i) + " attempts")
-        print("Rule 1 was " + Rec.RULE)
-        print("Rule 2 was " + Rec2.RULE)
-        print("Watched was " + get_names(watched) + ", Children were " + get_names(children) + ", num_extras was " + str(num_extras))
-        print("Graph 1 was " + get_names(graph1))
-        print("Graph 2 was " + get_names(graph2))
-        break
-    else:
-        for movie in graph1:
-            if movie not in graph2:
-                are_same = False
-        if not are_same:
-            print("Failed after " + str(i) + " attempts")
+        # compare the graphs
+        if len(graph1) != len(graph2):
+            are_same = False
+            print("Failed after " + str(i+1) + " attempts")
             print("Rule 1 was " + Rec.RULE)
             print("Rule 2 was " + Rec2.RULE)
-            print("Watched was " + get_names(watched) + ", Children were " + get_names(children) + ", num_extras was " + str(num_extras))
+            print("watched = " + get_names(watched1) + ", children = " + get_names(children1) + ", num_extras = " + str(num_extras))
             print("Graph 1 was " + get_names(graph1))
             print("Graph 2 was " + get_names(graph2))
             break
+        else:
+            for movie1 in graph1:
+                are_same = False
+                for movie2 in graph2:
+                    if movie1.name == movie2.name:
+                        are_same = True
+            if not are_same:
+                print("Failed after " + str(i+1) + " attempts")
+                print("Rule 1 was " + Rec.RULE)
+                print("Rule 2 was " + Rec2.RULE)
+                print("watched = " + get_names(watched1) + ", children = " + get_names(children1) + ", num_extras = " + str(num_extras))
+                print("Graph 1 was " + get_names(graph1))
+                print("Graph 2 was " + get_names(graph2))
+                break
 
-print("Succeeded")
-#Graph 1 was ['Doctor Strange', 'Guardians of the Galaxy Vol. 2', 'Captain Marvel', 'Ant-Man', 'Guardians of the Galaxy', 'Avengers: Age of Ultron']
-#Graph 2 was ['Doctor Strange', 'Guardians of the Galaxy Vol. 2', 'Captain Marvel', 'Ant-Man', 'Guardians of the Galaxy', 'The Avengers']
-# films = all_films.copy()
-# watched = [Rec.MOVIES['Doctor Strange'], Rec.MOVIES['Guardians of the Galaxy Vol. 2']]
-# children = [Rec.MOVIES['Captain Marvel'], Rec.MOVIES['Ant-Man']]
-# num_extras = 2
-#num_watched = math.floor(random() * len(all_films) / 3)
-#num_children = 1 + math.floor(random() * len(all_films) / 3)
-#num_extras = math.floor(random() * len(all_films) / 3)
+    if are_same:
+        print("Succeeded")
+
 '''num_watched = 2
 num_children = 2
 for num in range(num_watched):
@@ -97,12 +91,35 @@ for num in range(num_children):
     children.append(Rec.MOVIES[films[n]])
     films.remove(films[n])'''
 
-# Rec.set_rule("Relevant")
-# graph1 = Rec.find_best_subgraph(watched, children, num_extras)
-# Rec.set_rule("Recent")
-# graph2 = Rec.find_best_subgraph(watched, children, num_extras)
-# Rec.set_rule("Relevant")
-# graph3 = Rec.find_best_subgraph(watched, children, num_extras)
-# print("graph 1 is " + get_names(graph1))
-# print("graph 2 is " + get_names(graph2))
-# print("graph 3 is " + get_names(graph3))
+def test_specific_example(num_extras, watched, children):
+    watched1 = []
+    children1 = []
+    watched2 = []
+    children2 = []
+    for name in watched:
+        watched1.append(Rec.MOVIES[name])
+    for name in children:
+        children1.append(Rec.MOVIES[name])
+    for name in watched:
+        watched2.append(Rec2.MOVIES[name])
+    for name in children:
+        children2.append(Rec2.MOVIES[name])
+    Rec.set_rule("Relevant")
+    graph1 = Rec.find_best_subgraph(watched1, children1, num_extras)
+    Rec2.set_rule("Relevant")
+    graph2 = Rec2.find_best_subgraph(watched2, children2, num_extras)
+    print("graph 1 is " + get_names(graph1))
+    print("graph 2 is " + get_names(graph2))
+
+Rec.import_weighted_from_csv()
+Rec.import_data_from_csv()
+Rec2.import_weighted_from_csv()
+Rec2.import_data_from_csv()
+
+watched = ['Iron Man 2', 'Iron Man', 'Avengers: Age of Ultron', 'Captain America: The Winter Soldier', 'The Avengers', 'Ant-Man', 'Guardians of the Galaxy']
+children = ['Doctor Strange', 'Black Panther', 'Iron Man 3']
+
+extras = 5
+# test_specific_example(extras, watched, children)
+
+test_all_films()
